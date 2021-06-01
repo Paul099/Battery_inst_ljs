78e68a4bac08d9783c3d10035c16e87dbe49dcc0
Paul099 <messi1199@163.com>
2021-06-01 18:01:49 +0800

    使用梯形面积的方式（因为是折线）来求电量的积分

--- management_show.py
+++ management_show.py
@@ -37,6 +37,7 @@ class MyWidget(QtWidgets.QMainWindow,jiemiang.Ui_Form):
 
         self.vol_record1 = [0]
         self.cur_record1 = [0]
+        self.end_voltage =10.00
 
         '''添加线程和time.sleep()作为定时器'''
         self.update_para = threading.Thread(target=self.outputPower_start_shiebei1)
@@ -65,6 +66,16 @@ class MyWidget(QtWidgets.QMainWindow,jiemiang.Ui_Form):
         return sum
 
 
+    def sum_content(self,vol_list,time_list):
+        '''计算电量方法2，使用两个列表，遍历计算电量积分'''
+        battery_content = []
+        #i = 0
+        for i in range(len(vol_list)):
+            voltage_mid =(vol_list[i+1]-vol_list[i])/2
+            duration_time = time_list[i+1]-i
+            battery_content.append(duration_time/3600*voltage_mid*1000)
+        battery_content_sum =self.add_sum_para_show(battery_content)
+        return battery_content_sum
 
     def inputPower_set_max_current(self,):
         '''设定输入电流值'''
@@ -81,7 +92,7 @@ class MyWidget(QtWidgets.QMainWindow,jiemiang.Ui_Form):
         cmd[6] = (int_current & 0xFF000000) >> 24
 
         cmd[25] = self.add_sum_check_bit(cmd)
-        print('inputPower_set_current:cmd[25]',cmd[25])
+        #print('inputPower_set_current:cmd[25]',cmd[25])
         # self.ser = serial.Serial("COM5", 9600, timeout=5)
         # self.ser.write(cmd)
         #self.my_login.ser.write(cmd)
@@ -93,45 +104,70 @@ class MyWidget(QtWidgets.QMainWindow,jiemiang.Ui_Form):
         '''设备1放电'''
         if mylogin.ser.is_open:
             print('串口5打开！开始放电显示')
-        while True:
-            time.sleep(0.25)
-            #后面用服务器返回的数据来取代设定的大小
-            cmd = [0x00, ] * 26
-            # cmd=[0xaa,0x00,1,1,1,1,1,1]
-            cmd[0] = 0xaa
-            cmd[2] = 0x5F#5F读取相应的实时电压值
-            cmd[25] = self.add_sum_check_bit(cmd)
-            mylogin.ser.write(cmd)
-            time.sleep(2)#等待串口返回数据
+            self.outputStateLabel1.setText('放电中！')
+            output1_starttime = time.time()
+            output_content_sum =0
+            self.current_time = time.time()
+            self.cur_current =0
+            self.vol_current = 12
+            #self.current_time1 = []
+            while True:
+                time.sleep(0.25)
+                #后面用服务器返回的数据来取代设定的大小
+                cmd = [0x00, ] * 26
+                # cmd=[0xaa,0x00,1,1,1,1,1,1]
+                cmd[0] = 0xaa
+                cmd[2] = 0x5F#5F读取相应的实时电压值
+                cmd[25] = self.add_sum_check_bit(cmd)
+                mylogin.ser.write(cmd)
+                time.sleep(0.1)#等待串口返回数据
 
-            #if mylogin.ser.in_waiting:#有返回数据
-            '''将返回的串口数据4-7字节返回'''
+                '''使用循环记录上次的时间和电流值'''
+                last_cur_current =self.cur_current
+                last_current_time = self.current_time
+                #if mylogin.ser.in_waiting:#有返回数据
+                '''将返回的串口数据4-7字节返回'''
 
-            vol_sum = []
-            #self.vol_record1 = [0,]#存储数据
-            vol_sum.append(mylogin.STRGLO[3] & 0x000000FF)
-            vol_sum.append((mylogin.STRGLO[4] & 0x000000FF) << 8)
-            vol_sum.append((mylogin.STRGLO[5] & 0x000000FF) << 16)
-            vol_sum.append((mylogin.STRGLO[6] & 0x000000FF) << 24)
-            self.vol_current = self.add_sum_para_show(vol_sum)/1000#显示电压值
-            self.vol_record1.append(self.vol_current)#存储的电压值
-            print('self.vol_current',self.vol_current)
+                vol_sum = []
+                #self.vol_record1 = [0,]#存储数据
+                vol_sum.append(mylogin.STRGLO[3] & 0x000000FF)
+                vol_sum.append((mylogin.STRGLO[4] & 0x000000FF) << 8)
+                vol_sum.append((mylogin.STRGLO[5] & 0x000000FF) << 16)
+                vol_sum.append((mylogin.STRGLO[6] & 0x000000FF) << 24)
+                self.vol_current = self.add_sum_para_show(vol_sum)/1000#显示电压值
+                self.vol_record1.append(self.vol_current)#存储的电压值
 
-            cur_sum = []
-            #self.cur_record1 = [0,]#存储数据
-            cur_sum.append(mylogin.STRGLO[7] & 0x000000FF)
-            cur_sum.append((mylogin.STRGLO[8] & 0x000000FF) << 8)
-            cur_sum.append((mylogin.STRGLO[9] & 0x000000FF) << 16)
-            cur_sum.append((mylogin.STRGLO[10] & 0x000000FF) << 24)
-            self.cur_current = self.add_sum_para_show(cur_sum)/1000#显示电压值
-            self.cur_record1.append(self.cur_current)#存储的电流值
-            print('self.cur_current',self.cur_current)
+                #print('self.vol_current',self.vol_current)
+
+                cur_sum = []
+                #self.cur_record1 = [0,]#存储数据
+                cur_sum.append(mylogin.STRGLO[7] & 0x000000FF)
+                cur_sum.append((mylogin.STRGLO[8] & 0x000000FF) << 8)
+                cur_sum.append((mylogin.STRGLO[9] & 0x000000FF) << 16)
+                cur_sum.append((mylogin.STRGLO[10] & 0x000000FF) << 24)
+                self.cur_current = self.add_sum_para_show(cur_sum)/1000#显示电压值
+                self.cur_record1.append(self.cur_current)#存储的电流值
+                #print('self.cur_current',self.cur_current)
+                # self.current_time1.append(time.time())
 
 
-            self.outputVoltageEdit1.setText(str(self.vol_current))#显示电压
-            self.outputCurrentEdit1.setText(str(self.cur_current))#显示电流
-            self.outputEndVoltageEdit1.setText(str(10.00))  # 暂时写死从数据库读取
+                self.current_time = time.time()#获取当前电流采集的时间
+                output_content1 = (last_cur_current +  self.cur_current)/2
+                output_content_duration = self.current_time-last_current_time
+                output_content_sum =(output_content1*output_content_duration + output_content_sum)*1000/3600#考虑到折线图，采用的梯形面积计算
 
+                print("output_content1:%s,output_content_duration:%s,output_content_sum:%f"%(output_content1,output_content_duration,output_content_sum))
+                self.outputVoltageEdit1.setText(str(self.vol_current))#显示电压
+                self.outputCurrentEdit1.setText(str(self.cur_current))#显示电流
+                self.outputEndVoltageEdit1.setText(str(self.end_voltage))  # 暂时写死从数据库读取
+
+                if self.vol_current < self.end_voltage:
+                    '''判断放电结束'''
+                    self.outputStateLabel1.setText('放电结束！')
+                    self.outputContentEdit1.setText(format(output_content_sum,'.2f'))#输出总容量
+                    #self.textEdit1.setReadOnly(format(output_content_sum,'.2f'))
+                    print('output_content_sum',output_content_sum)
+                    break
 
 
     def graph_show(self):
@@ -147,7 +183,7 @@ class MyWidget(QtWidgets.QMainWindow,jiemiang.Ui_Form):
         self.pw.setBackground('w')
 
         # 设置Y轴 刻度 范围
-        self.pw.setYRange(-10,10)  # 最大值
+        self.pw.setYRange(0,20)  # 最大值
         #self.pw.setXRange(0, 3000)
 
         # 居中显示 PlotWidget
@@ -170,7 +206,7 @@ class MyWidget(QtWidgets.QMainWindow,jiemiang.Ui_Form):
         '''
         print('放电电压显示输出定时器启动！')
         while True:
-            print('self.vol_record',self.vol_record1)
+            #print('self.vol_record',self.vol_record1)
             self.curve.setData(list(range(len(self.vol_record1))), self.vol_record1, linewidth=1)
             time.sleep(0.25)
 
